936221381c39953f0e49fc1857fd11faa501cea9
wrightbradley <bradley.wright.tech@gmail.com>
2023-10-16 14:56:25 -0400

    feat: add istio and dashboard helm deployments
    Signed-off-by: wrightbradley <bradley.wright.tech@gmail.com>

---
+++ scripts/setup-dashboard.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard --set fullnameOverride='kubernetes-dashboard'
---
+++ scripts/setup-istio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+helm repo add istio https://istio-release.storage.googleapis.com/charts
+helm repo update
+
+kubectl create namespace istio-system
+helm install istio-base istio/base -n istio-system --wait
+helm install istiod istio/istiod -n istio-system --wait
+
+kubectl create namespace istio-ingress
+kubectl label namespace istio-ingress istio-injection=enabled
+helm install istio-ingressgateway istio/gateway -n istio-ingress --wait
